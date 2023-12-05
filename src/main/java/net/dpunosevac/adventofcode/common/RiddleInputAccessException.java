c34098dc9419e3ae16e0c76707af5749f4e695dc
Dusan Punosevac <dpunosevac@foursquare.com>
2023-12-05 17:17:15 +0100

    Clean slate

--- src/main/java/net/dpunosevac/adventofcode/common/RiddleInputAccessException.java
+++
@@ -1,8 +0,0 @@
-package net.dpunosevac.adventofcode.common;
-
-class RiddleInputAccessException extends RuntimeException {
-
-  RiddleInputAccessException(Throwable cause) {
-    super("Cannot access RiddleInput", cause);
-  }
-}
